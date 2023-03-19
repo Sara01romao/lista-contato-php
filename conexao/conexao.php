b4adc2b6499a957a6a8572d1cf229bb343c90d44
Sara Romão <sara-romao@live.com>
2023-03-18 23:36:45 -0300

    feat:conexao

---
+++ conexao/conexao.php
@@ -0,0 +1,11 @@
+<?php
+$servername= "localhost";
+$database= "";
+$username = "root";
+$password= "";
+
+//criação da conexao 
+$conexao = mysqli_connect($servername, $username, $password , $database);
+
+
+?>
