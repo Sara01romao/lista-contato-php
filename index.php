ecc098eebac7a0b5a2f91fdc5c7a3c3bbc1f9812
Sara Romão <sara-romao@live.com>
2023-03-19 23:00:00 -0300

    feat:inicio layout lista

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+	
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <title>Document</title>
+</head>
+<body>
+
+   <section class="lista-container">
+        <div class="contato-item">
+            <p>nome </p>
+            <div>
+                <div>
+                    <button type="button">Editar</button>
+                    <button type="button">Excluir</button>
+                </div>
+            </div>
+        </div>
+   </section>
+    
+      
+
+
+     
+</body>
+</html>
