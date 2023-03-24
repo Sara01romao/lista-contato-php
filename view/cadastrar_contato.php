c8043aeaa899c8038322b307f617c76c8c798454
Sara Romão <sara-romao@live.com>
2023-03-23 23:50:00 -0300

    feat:estrutura inicial pg de cadastro

---
+++ view/cadastrar_contato.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <title>Cadastrar Cliente</title>
+</head>
+<body>
+
+   <form action="">
+       <label for="">Nome</label>
+       <input type="text">
+
+       <label for="">Telefone</label>
+       <input type="text">
+
+       <label for="">Email</label>
+       <input type="text">
+   </form>
+
+
+     
+</body>
+</html>
